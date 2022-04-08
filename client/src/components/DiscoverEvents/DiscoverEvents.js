6e613461525c7323264cf246cf5821959928dc4f
Alison Melendez <alisonmelendez132@gmail.com>
2022-04-08 16:48:19 -0400

    added a working modal and contains a form

--- client/src/components/DiscoverEvents/DiscoverEvents.js
+++ client/src/components/DiscoverEvents/DiscoverEvents.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../DiscoverEvents/DiscoverEvents.scss'; 
+import Modal from '../Modal/Modal';
 
 function DiscoverEvents(){ 
+
+    const[isOpen, setIsOpen] = useState(false); 
+
+    function randomFunction(){
+        //modal will pop up here as it is rendered on click of the plus sign 
+        console.log('I was clicked woo'); 
+        
+    }
 
     return (
         <>
@@ -11,7 +20,9 @@ function DiscoverEvents(){
                 <h3 className="eventHeader">Event Title</h3>
                 <p className="eventInfo">Event Short Bio</p>
             {/* </div>  */}
-            <p className="plusSign">+</p>
+            <button onClick={() => setIsOpen(true)}className="plusSign">+</button>
+
+            <Modal open={isOpen} onClose={() => setIsOpen(false)}/>
         </div>
         </>
     ); 
