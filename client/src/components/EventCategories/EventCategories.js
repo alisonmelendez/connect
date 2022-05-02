7e4dc8e52fd05459f35185bc0c11d876654c4be9
Alison Melendez <alisonmelendez132@gmail.com>
2022-05-02 16:33:07 -0400

    additional change

--- client/src/components/EventCategories/EventCategories.js
+++ client/src/components/EventCategories/EventCategories.js
@@ -3,15 +3,6 @@ import EventCategoriesCard from '../EventCategoriesCard/EventCategoriesCard';
 import './EventCategories.scss'
 
 function EventCategories({ setFilteredCategories, handleCategoryChange }){ 
-
-
-    // useEffect(() => {
-    //     fetch("https://app.ticketmaster.com/discovery/v2/classifications.json?apikey=shAfe86LVSVWkdRIRrG3BUq1N13kRA22&size=6")
-    //       .then((r) => r.json())
-    //       .then((data) => setCategories(data._embedded.classifications)); 
-    //   }, []);
-
-    // categories.map((category)=> console.log(category.segment.name))
     
     const categories = ["All", "Arts & Theatre", "Music", "Sports"]
 
